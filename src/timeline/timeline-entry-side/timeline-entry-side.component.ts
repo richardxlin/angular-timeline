d0c561e2928cd1862e65f3efb15ea2b8e009dc2f
Richard Lin <richard@540.co>
2020-01-10 13:03:26 -0500

    update with service to track dates

--- src/timeline/timeline-entry-side/timeline-entry-side.component.ts
+++ src/timeline/timeline-entry-side/timeline-entry-side.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, ElementRef, ViewEncapsulation } from '@angular/core';
+import { TimelineService } from '../services/timeline.service';
+
 
 @Component({
   selector: 'ng-timeline-entry-side',
@@ -15,7 +17,21 @@ export class NgTimelineEntrySideComponent {
   set mobile(value) {
     this.elementRef.nativeElement.classList.toggle('mobile', value);
   }
+
+  uniqueYears = new Set();
   
-  constructor(private elementRef: ElementRef) { }
+  constructor(private elementRef: ElementRef, private timelineService: TimelineService) {}
+
+  ngAfterContentInit()	{
+    console.log('change', this.elementRef.nativeElement.innerText)
+    const year = this.elementRef.nativeElement.innerText;
+    const yearExists = this.timelineService.hasYear(year);
+    if (yearExists) {
+      this.elementRef.nativeElement.remove();
+    } else {
+      this.timelineService.addYear(year.trim());
+    }
+  }
+
 
 }
