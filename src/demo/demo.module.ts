45509e8e5274d3284dff83bc147b41ccec2707fa
Richard Lin <richard@540.co>
2020-01-10 11:51:29 -0500

    update

--- src/demo/demo.module.ts
+++ src/demo/demo.module.ts
@@ -3,6 +3,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { DemoComponent } from './demo.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -10,8 +11,10 @@ import { DemoComponent } from './demo.component';
   ],
   imports: [
     BrowserModule,
+    MatIconModule,    
     BrowserAnimationsModule,
-    NgTimelineModule
+    NgTimelineModule,
+    
   ],
   providers: [],
   bootstrap: [DemoComponent]
